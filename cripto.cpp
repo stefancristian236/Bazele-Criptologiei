843fcaf9044ad8e958168c84a3f2984ae3f91c49
stefancristian236 <cristicristi532@gmail.com>
2026-03-13 13:17:45 +0200

    made the header functional

--- cripto.cpp
+++ cripto.cpp
@@ -2,20 +2,19 @@
 #include <iostream>
 #include <map>
 
-class __FileOperator__ {
-    private:
-        std::string text;
-    public:
-        __FileOperator__ (std::string auxText) {text = auxText;} //define constructor
+__FileOperator__::__FileOperator__(std::string auxText) {
+    text = auxText;
+}
 
-        void __CheckFrequency__ () {
-            std::cout << "Frecventa caracterelor:" << std::endl;
-            std::map <char, int> frequency_map;
-            for (const auto ch : text) {
-                frequency_map[ch]++;
-            }
-            for (const auto it : frequency_map) {
-                std::cout << it.first << ":" << it.second << std::endl;
-            }
-        }
-};
+void __FileOperator__::__CheckFrequency__() {
+    std::cout << "Frecventa caracterelor:" << std::endl;
+    std::map<char, int> frequency_map;
+
+    for (const auto ch : text) {
+        frequency_map[ch]++;
+    }
+
+    for (const auto it : frequency_map) {
+        std::cout << it.first << ":" << it.second << std::endl;
+    }
+}
--- cripto.h
+++ cripto.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
 #include <fstream>
 
-class __FileOperator__{
+class __FileOperator__ {
     private:
         std::string text;
     public:
-        __FileOperator__ (std::string auxText){};
-        void __CheckFrequency__(){};
+        __FileOperator__ (std::string auxText);
+        void __CheckFrequency__();
 };
